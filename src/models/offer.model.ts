9a55ac0c3958091a66d2784f9e798084912676bf
Dominik Noga <dominik.noga@felten-group.com>
2024-09-05 08:44:27 +0200

    Added offer routes and handlers

---
+++ src/models/offer.model.ts
@@ -0,0 +1,12 @@
+import {z} from 'zod';
+
+const OfferSchema = z.object({
+    id: z.string(),
+    userId: z.string(),
+    title: z.string(),
+    description: z.string(),
+    images: z.array(z.string()),
+    creationDate: z.date()
+});
+
+export type Offer = z.infer<typeof OfferSchema>;
