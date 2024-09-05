8b6464972b8f5171c19cbe1290a414fec6364c66
Dominik Noga <dominik.noga@felten-group.com>
2024-09-05 11:12:53 +0200

    Added BE for images upload

--- src/utils/helpers/createApp.ts
+++ src/utils/helpers/createApp.ts
@@ -2,14 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import usersRouter from '../../routes/users.routes';
 import offersRouter from '../../routes/offers.routes';
+import imagesRouter from '../../routes/images.routes';
 
 export function createApp() {
   const app = express();
   app.use(cors());
   app.use(express.urlencoded({extended: false}));
   app.use(express.json());
+  app.use('/resources', express.static('resources'));
   app.use('/api/users', usersRouter);
-  app.use('api/offers', offersRouter)
+  app.use('/api/offers', offersRouter);
+  app.use('/api/images', imagesRouter);
 
   return app;
 }
